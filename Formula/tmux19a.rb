550ecdb5e17ebc2d9610cc0d7bff80cc5e4cf596
Mauro Verrocchio <mauroverrocchio@gmail.com>
2017-02-02 12:09:51 +0100

    fix formula #2

--- Formula/tmux19a.rb
+++ Formula/tmux19a.rb
@@ -5,14 +5,6 @@ class Tmux19a < Formula
   url 'https://github.com/tmux/tmux/releases/download/1.9a/tmux-1.9a.tar.gz'
   sha256 'c5e3b22b901cf109b20dab54a4a651f0471abd1f79f6039d79b250d21c2733f5'
 
-  head do
-    url 'git://github.com/tmux/tmux.git'
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
   depends_on 'pkg-config' => :build
   depends_on 'libevent'
 
